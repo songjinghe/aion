d6c127a0035f7bae463cb63ae22abccd5ecc0e26
lirasndfs <109637952+Iirasndfs@users.noreply.github.com>
2026-02-14 19:56:04 +0800

    Update Main.java

--- community/temporal-procs/src/main/java/org/neo4j/temporalprocs/Main.java
+++ community/temporal-procs/src/main/java/org/neo4j/temporalprocs/Main.java
@@ -18,7 +18,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package org.neo4j.temporalprocs;
-
+//package org.example;
 import static org.neo4j.configuration.GraphDatabaseSettings.DEFAULT_DATABASE_NAME;
 
 import java.io.IOException;
@@ -26,16 +26,23 @@ import java.nio.file.Path;
 import java.util.Scanner;
 import java.util.concurrent.locks.LockSupport;
 
+import org.neo4j.common.DependencyResolver;
 import org.neo4j.configuration.GraphDatabaseSettings;
 import org.neo4j.configuration.connectors.BoltConnector;
 import org.neo4j.configuration.helpers.SocketAddress;
 import org.neo4j.dbms.api.DatabaseManagementService;
+import org.neo4j.dbms.api.DatabaseManagementServiceBuilder;
+import org.neo4j.graphdb.GraphDatabaseService;
 import org.neo4j.harness.Neo4jBuilders;
 import org.neo4j.io.fs.FileSystemAbstraction;
 import org.neo4j.io.pagecache.PageCache;
+import org.neo4j.kernel.api.procedure.GlobalProcedures;
+import org.neo4j.kernel.internal.GraphDatabaseAPI;
 import org.neo4j.temporalgraph.lineageindex.EntityLineageTracker;
 import org.neo4j.temporalgraph.timeindex.SnapshotCreationPolicy;
 import org.neo4j.temporalgraph.timeindex.timestore.TimeBasedTracker;
+import org.neo4j.temporalprocs.LineageStoreProcedures;
+import org.neo4j.temporalprocs.TimeStoreProcedures;
 
 public class Main {
 
@@ -45,20 +52,19 @@ public class Main {
 
     public static void main(String[] args) throws IOException, InterruptedException {
 
-        var embeddedDatabaseServer = Neo4jBuilders.newInProcessBuilder(DB_PATH)
-                .withProcedure(LineageStoreProcedures.class)
-                .withProcedure(TimeStoreProcedures.class)
-                .withConfig(BoltConnector.enabled, true)
-                .withConfig(BoltConnector.listen_address, new SocketAddress("0.0.0.0", 7687))
+        var embeddedDatabaseServer = new DatabaseManagementServiceBuilder(DB_PATH)
+                .setConfig(BoltConnector.enabled, true)
+                .setConfig(BoltConnector.listen_address, new SocketAddress("localhost", 7687))
                 .build();
-
+        GraphDatabaseService db = embeddedDatabaseServer.database(DEFAULT_DATABASE_NAME);
+        registerProcedures(db);
         registerTracker(
-                embeddedDatabaseServer.databaseManagementService(),
-                embeddedDatabaseServer.config().get(GraphDatabaseSettings.neo4j_home),
+                embeddedDatabaseServer,
+                DB_PATH,
                 0);
         registerTracker(
-                embeddedDatabaseServer.databaseManagementService(),
-                embeddedDatabaseServer.config().get(GraphDatabaseSettings.neo4j_home),
+                embeddedDatabaseServer,
+                DB_PATH,
                 1);
 
         var input = new Scanner(System.in);
@@ -69,8 +75,8 @@ public class Main {
                 long lastTimeOfTimeStore = timeBasedTracker.getLastCommittedTime();
                 long lastTxIdOfLineageStore = lineageTracker.getLastTransactionId();
                 long lastTimeOfLineageStore = lineageTracker.getLastCommittedTime();
-                System.out.printf("TimeStore: lastTxId %ld, lastTime %ld; LineageStore: lastTxId %ld, lastTime %ld.%n", 
-                    lastTxIdOfTimeStore, lastTimeOfTimeStore, lastTxIdOfLineageStore, lastTimeOfLineageStore);
+                System.out.printf("TimeStore: lastTxId %ld, lastTime %ld; LineageStore: lastTxId %ld, lastTime %ld.%n",
+                        lastTxIdOfTimeStore, lastTimeOfTimeStore, lastTxIdOfLineageStore, lastTimeOfLineageStore);
                 Thread.sleep(120_000);
             }
         } catch (InterruptedException e){
@@ -78,10 +84,22 @@ public class Main {
         }
 
         closeTrackers();
-        embeddedDatabaseServer.close();
+        embeddedDatabaseServer.shutdown();
         System.out.println("DB Server closed. process exit.");
     }
+    private static void registerProcedures(GraphDatabaseService db) {
+        try {
+            DependencyResolver resolver = ((GraphDatabaseAPI) db).getDependencyResolver();
+            GlobalProcedures procedures = resolver.resolveDependency(GlobalProcedures.class);
+            procedures.registerProcedure(LineageStoreProcedures.class);
+            procedures.registerProcedure(TimeStoreProcedures.class);
+//            procedures.registerFunction(LineageStoreProcedures.class);
+//            procedures.registerFunction(TimeStoreProcedures.class);
 
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
     private static void registerTracker(DatabaseManagementService dbms, Path dbPath, int type) throws IOException {
         var pageCache = (PageCache) dbms.database(DEFAULT_DATABASE_NAME).getPageCache();
         var fs = (FileSystemAbstraction) dbms.database(DEFAULT_DATABASE_NAME).getFileSystem();
